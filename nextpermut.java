73917ac0cb5a43a803752db488b600ee09b442fb
Apoorva Aware <115568209+aapurwah@users.noreply.github.com>
2025-03-29 19:08:18 +0530

    Update nextpermut.java

---
+++ nextpermut.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class nextpermut {
+
+    public static void nextpt(int[] nums) {
+        int n = nums.length;
+        int i = n - 2;
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+        if(i >= 0) {
+            int j = n - 1;
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            swap(nums, i, j);
+        }
+        reverse(nums, i + 1, n - 1);
+    }
+
+        private static void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+        
+
+    
+
+
+}
+    
